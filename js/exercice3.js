b4117c0279400b31d18dadef54cc315d63b132ef
Vincent Peralta <peralta@uic.org>
2018-01-08 15:11:00 +0100

    a

---
+++ js/exercice3.js
@@ -0,0 +1,22 @@
+var a = 1;  // A Proscrire !!
+console.log(a);
+
+
+// let & const
+
+let b = 2;
+const c = 3;
+
+// c = 4; // ERROR
+
+console.log(b);
+console.log(c);
+
+{
+    var d =  2;
+    let e = 2;
+}
+
+
+console.log(d);
+console.log(e);
---
+++ js/exercice4.js
@@ -0,0 +1,33 @@
+let toto = 2;
+
+// JSON
+
+let a = {"nb" : 2};
+console.log(a.nb);
+
+a = {"nb" : 2, "name" : "felix"}
+console.log(a.nb + " " + a.name);
+
+let b = {"user" : a};
+console.log("name = " + b.user.name);
+
+let c = {"user" : {"nb" : 3, "name" : "Aurélie"}};
+console.log(c);
+
+let tab1 = [1, 2 ,3];
+
+console.log("0 = " + tab1[0]);
+console.log("1 = " + tab1[1]);
+console.log("2 = " + tab1[2]);
+console.log("Taille = " + tab1.length);
+
+let tabUser = [b, c];
+console.log("user0 = " + tabUser[0]);
+
+
+console.log(b);
+b.valid = true;
+console.log(b);
+
+b.parent = [ c ];
+console.log(b);
